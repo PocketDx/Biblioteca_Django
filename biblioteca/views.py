86c37edd1c893592d28527fb506065f1c23b2b5d
PocketDx <dairojavierrg@ufps.edu.co>
2025-04-25 08:42:56 -0500

    Issues Solve
    Definicion de Router, imports para el funcionamiento de Action y AVG

--- biblioteca/views.py
+++ biblioteca/views.py
@@ -1,5 +1,31 @@
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django.db.models import Avg
 from .models import Autor, Libro, Resena
-from .serializer import Autor, Libro, Resena
+from .serializer import AutorSerializer, LibroSerializer, ResenaSerializer
 
 # Create your views here.
+
+class AutorViewSet(viewsets.ModelViewSet): # Vista para el modelo Autor
+    queryset = Autor.objects.all()
+    serializer_class = AutorSerializer # Serializador para el modelo Autor
+
+class LibroViewSet(viewsets.ModelViewSet): # Vista para el modelo Libro
+    queryset = Libro.objects.all()
+    serializer_class = LibroSerializer # Serializador para el modelo Libro
+
+    def get_queryset(self):
+        if self.request.query_params.get('recent_reviews'):
+            return self.queryset.order_by('-fecha_publicacion')[:10]
+        return Libro.objects.all()
+    
+    @action(detail=True, methods=['get'])
+    def average_rating(self, request, pk=None):
+        libro = self.get_object()
+        avg_rating = libro.resenas.aggregate(Avg('calificacion'))['calificacion__avg']
+        return Response({'average_rating': 0})
+
+class ResenaViewSet(viewsets.ModelViewSet): # Vista para el modelo Reseña
+    queryset = Resena.objects.all()
+    serializer_class = ResenaSerializer # Serializador para el modelo Reseña
--- biblioteca_project/urls.py
+++ biblioteca_project/urls.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from biblioteca.views import AutorViewSet, LibroViewSet, ResenaViewSet
+
+router = DefaultRouter()
+router.register(r'autores', AutorViewSet, basename='autor')
+router.register(r'libros', LibroViewSet, basename='libro')
+router.register(r'resenas', ResenaViewSet, basename='resena')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)), # Se incluye la URL del router
 ]
